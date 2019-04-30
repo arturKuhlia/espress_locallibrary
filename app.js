9b1a168547a285ea86a5475d10e992cc2fd797d9
arturKuhlia <49421043+arturKuhlia@users.noreply.github.com>
2019-04-30 00:03:02 -0400

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 
 var mongoose = require('mongoose');
 
-var mongoDB = 'mongodb+srv://arturshevtsov:Vrat2533@contractdb1-7a34e.gcp.mongodb.net/test?retryWrites=true';
+var mongoDB = 'mongodb+srv://user:password123@cluster0-mvv6u.mongodb.net/test?retryWrites=true';
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
